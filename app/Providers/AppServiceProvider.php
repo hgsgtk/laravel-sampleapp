8d27219e4d7f1bd193a4a68990812e28fb8ed4e4
Khigashiguchi <econoics0it0bem0116@gmail.com>
2018-09-30 16:37:05 +0900

    chapter2 completed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use App\BlowfishEncrypter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('encrypter', function ($app) {
+            $config = $app->make('config')->get('app');
+
+            if (Str::startsWith($key = $this->key($config), 'base64:')) {
+                $key = base64_encode(substr($key, 7));
+            }
+
+            return new BlowfishEncrypter($key);
+        });
+    }
+
+    protected function key(array $config)
+    {
+        return tap($config['key'], function ($key) {
+            if (empty($key)) {
+               throw new RuntimeException('No application encryption key has been specified.');
+            }
+        });
     }
 }
